ba30af1c8db8b99716fd037bfd8b72bd26e4d73e
Eric Chiu <42273490+Eric07110904@users.noreply.github.com>
2021-08-24 20:34:43 +0800

    add Data Augmentation method2

--- augmentation.py
+++ augmentation.py
@@ -1,26 +1,117 @@
-import glob 
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
+import random, glob, datetime, argparse, os
+from utils import draw_all
+
+# configuration
 label = ['normal', 'IEM', 'Absent', 'Fragmented']
 label_dict = {"normal":[],"IEM":[],"Absent":[],"Fragmented":[]}
+file_dict = {"normal":[],"IEM":[],"Absent":[],"Fragmented":[]}
+swallow_dict = {} # {"ID":["Normal","Normal"......]}
+type_template = {
+    "normal":['Normal', 'Normal', 'Normal', 'Normal', 'Normal', 'Normal', 'Normal', 'Normal', 'Normal', 'Normal'],
+    "IEM":['Failed', 'Weak', 'Weak', 'Weak', 'Weak', 'Failed', 'Failed', 'Failed', 'Weak', 'Failed'],
+    "absent":['Failed', 'Failed', 'Failed', 'Failed', 'Failed', 'Failed', 'Failed', 'Failed', 'Failed', 'Failed']
+}
+
+# parser 
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--number', type=int, default='10')
+    
+    return parser
 
 def read_data(filename, true_label):
     df = pd.read_csv(filename, encoding='big5')
     ans = list(np.where(df['檢查流程']!='None')[0])
-    print(filename, true_label)
-    # print(ans, len(ans))
-    print(df.iloc[ans])
     swallow_status = [df.iloc[ans[i]]['Contraction vigor'] for i in range(len(ans))][:-1]
-    # test_names = [df.iloc[ans[i]]["檢查流程"] for i in range(len(ans))][:-1]
     label_dict[true_label].append(swallow_status)
-    print(swallow_status)
-    
+    swallow_dict[filename] = swallow_status
 
-res = glob.glob('./train/*.csv')
+def get_swallow_range(df):
+    swallow_names = ["Wet swallow"+str(i+1) for i in range(10)]
+    swallow_index = []
+    swallow_range = []
+    ans = list(np.where(df['檢查流程']!='None')[0])
+    for i in range(len(ans)):
+        test_name = df.iloc[ans[i]]['檢查流程'] # test_name ==> 檢測的名稱
+        if 'Wet swallow10' == test_name:
+            swallow_index.append(ans[i])
+            swallow_index.append(ans[i+1])
+            continue 
+        if test_name in swallow_names:
+            swallow_index.append(ans[i])
+    for i in range(len(swallow_index)-1):
+        swallow_range.append([swallow_index[i],swallow_index[i+1]-1])
+    swallow_status = [df.iloc[ans[i]]['Contraction vigor'] for i in range(len(ans))][:-1]
+    return swallow_range, np.array(swallow_status)
 
-for r in res:
-    file_label = r.split('-')[1][:-4]
-    read_data(r, file_label)
+def create_data(label):
+    datetime_dt = datetime.datetime.today()          # 獲得當地時間
+    datetime_str = datetime_dt.strftime("%Y%m%d%H%M%S")  # 格式化日期
+    print("[INFO] Create "+datetime_str+'-'+label+'.CSV')
+
+    type_number = len(file_dict[label])
+    df_list = []
+    # 隨機挑選10次swallow
+    i = 0
+    while i<10:
+        file_randn = random.randint(0,type_number-1)
+        fname = file_dict[label][file_randn]
+        
+        
+        target_stype = type_template[label][i] # Normal Failed Weak..... (swallow type )
+        df = pd.read_csv(fname, encoding='big5')
+        ans = list(np.where(df['檢查流程']!='None')[0])[:-1]
+        swallow_range, swallow_status = get_swallow_range(df)
+        type_list = np.where(swallow_status==target_stype)[0]
+
+        if len(type_list) == 0:
+            continue
+
+        swallow_randn = random.randint(0, len(type_list)-1)
+        target_swallow_index = type_list[swallow_randn]
+        print('swallow'+str(i+1)+':\t'+"|From "+fname+"\t"+"swallow"+str(target_swallow_index+1)+"\t")
+        target_swallow = swallow_range[target_swallow_index]
+        df_list.append(df.iloc[target_swallow[0]:target_swallow[1]+1])
+        i+=1
+
+
+    res = pd.concat(df_list)
+    res.to_csv(os.path.join('data augmentation2',datetime_str+'-'+label+'.csv'), encoding='big5')
+def draw():
+    res = glob.glob('./data augmentation2/*.csv')
+    for r in res:
+        df = pd.read_csv(r, encoding='big5')
+        sensors = [' P' + str(i) for i in range(1,21)] # 22個sensor p1~p22
+        print(r)
+        draw_all(df, r, sensors)
+
+if __name__ == "__main__":
+    if not os.path.exists('data augmentation2'):
+        os.makedirs('data augmentation2')
+        print("[INFO] Create data augmentation2 folder")
+
+    res = glob.glob('./train/*.csv')
+    parser = get_parser()
+    args = parser.parse_args()
+    number = args.number
+
+    for r in res:
+        file_label = r.split('-')[1][:-4]
+        file_dict[file_label].append(r)
+        read_data(r, file_label)
+
+    # Create fake HRM data 
+    for i in range(number):
+        create_data('normal')
+        print("\n")
+        create_data('IEM')
+        print("\n")
+
+    print("[INFO] Start drawing HRM curve")
+    draw()
+
 
 
