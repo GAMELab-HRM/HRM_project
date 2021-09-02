77944c489c101eaa20eb2b592b0b33d0d841ba03
Eric Chiu <42273490+Eric07110904@users.noreply.github.com>
2021-09-02 22:40:57 +0800

    add HRM custom dataset (pytorch)

--- VAE/data.py
+++ VAE/data.py
@@ -1,5 +1,7 @@
 from torch.utils.data.dataset import Dataset
 import torch, os, glob 
+import numpy as np 
+import pandas as pd 
 
 # config
 data_path = '../train/*.CSV'
@@ -12,14 +14,21 @@ def get_data(path):
         ans.append(t[:-4])
 
     return res, ans
-res, ans = get_data(data_path)
+
+def read_csv_data(path):
+    df = pd.read_csv(path, encoding='big5')
+    sensors = [' P' + str(i) for i in range(1,21)] # 22個sensor p1~p22
+    hrm = df[:][sensors].values
+    return hrm 
 
 # create custom dataset
 class HRMdataset(Dataset):
     def __init__(self, path):
         self.data, self.label = get_data(path)
-        pass 
+        
     def __getitem__(self, index):
-        return self.data[index], self.label[index]
+        hrm_data = read_csv_data(self.data[index])
+        return hrm_data, self.label[index]
     def __len__(self):
         return len(self.data)
+
