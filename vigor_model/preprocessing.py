e7a018874ac569eb0fd31e328f3e3cde3d818871
pony <pony8674312@gmail.com>
2021-09-07 15:42:03 +0800

    multiple class

--- vigor_model/preprocessing.py
+++ vigor_model/preprocessing.py
@@ -9,15 +9,17 @@ from sklearn.impute import SimpleImputer
 def mapping(param):
     def decorator(func):
         def wrap(x):
-            label = 1 if x in param else 0
-            return label
+            for key, value in param.items():
+                if x in value:
+                    return key
+            return 0
         return wrap
     return decorator
 
 
-def mapping_Y_label(df, target_lst):
+def mapping_Y_label(df, target_dict):
     
-    @ mapping(param=target_lst)
+    @ mapping(param=target_dict)
     def mapping_rule(x):
         pass
     
@@ -28,9 +30,7 @@ def mapping_Y_label(df, target_lst):
 
 def encode_data(df, **categorical_data):
     target_column = df['patient_type']
-    id_column = df['ID']
-
-    DCI_IRP_df = df.loc[:, df.columns[22:-1]]
+    DCI_IRP_df=df.loc[:, df.columns[21:-1]]
     LE = LabelEncoder()
     df_lst = []
 
@@ -43,32 +43,30 @@ def encode_data(df, **categorical_data):
 
     df = pd.concat(df_lst, axis=1)
     df['patient_type'] = target_column
-    df.insert(0, 'ID', id_column)
     
     return df
 
 
 def process_DCI_IRP(df):
-    id_column = df['ID']
-    df.drop(['ID'], axis=1, inplace=True)
     df.replace('-', 0, inplace=True)
     df = df.astype(float)
-    df.insert(0, 'ID', id_column)
 
     return df
-def one_hot_encoding(df):
-    col = ['v'+str(i+1) for i in range(10)] + ['p'+str(i+1) for i in range(10)]
-    df2 = pd.get_dummies(df,columns=col,prefix=col)
-    label = df2['patient_type']
-    df2 =  df2.drop(['patient_type'],axis=1)
-    df2 = pd.concat([df2,label],axis=1)
-    return df2
-    #df2.to_csv('./training_data/onehot.csv',encoding='big5',index=False)
+
+
+    
 
 if __name__ == '__main__':
     
     df = pd.read_csv('./data/all_patient.csv', encoding='big5', low_memory=False)
-    df = mapping_Y_label(df, ['normal'])
+    # 自訂label格式 -> class: label，label可以是list
+    # 自訂label從1開始，預設其他會被歸類為0
+
+    df = mapping_Y_label(df, {
+        '1': 'normal',
+        '2': 'IEM',
+    })
+
     categorical_data={
         # 因為list不能當key，所以先做成字串，放入encode_data()之後再轉回list
         ' '.join(['v'+ str(x) for x in range(1, 11)]): ['Failed', 'Weak', 'Normal'], 
@@ -77,5 +75,4 @@ if __name__ == '__main__':
     df = encode_data(df, **categorical_data)
 
     df = process_DCI_IRP(df)
-    df = one_hot_encoding(df)
     output('training_data', 'training.csv', df)
