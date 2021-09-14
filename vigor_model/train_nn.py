04f36989c397d01982ed9ccfa327413f30aae606
Eric Chiu <42273490+Eric07110904@users.noreply.github.com>
2021-09-14 23:45:07 +0800

    update

--- vigor_model/train_nn.py
+++ vigor_model/train_nn.py
@@ -1,25 +1,53 @@
-from torch import optim
 from model import HRMdataset, DatasetFromSubset, NN, train_normalization, test_normalization
 from torch.utils.data import DataLoader
 from torchsummaryX import summary
 from torch.autograd import Variable
 from qqdm import qqdm
-import torch 
+import torch, random
 import torch.nn as nn 
+import numpy as np 
+import torchmetrics
 import matplotlib.pyplot as plt
 
+# Seed
+seed = 123
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+np.random.seed(seed)
+random.seed(seed)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+
 # hyper params 
-BATCH_SIZE = 1
-epochs = 20
+BATCH_SIZE = 3
+epochs = 40
 lr = 1e-3
+class_number = 4
+metric_acc = torchmetrics.Accuracy()
+metric_valid_acc = torchmetrics.Accuracy()
+metric_acc.to("cuda")
+metric_valid_acc.to("cuda")
+
+def compute_accuracy2(dataset, model):
+    correct = 0
+    total = 0
+    model.eval()
+    temp_dataloader = DataLoader(dataset, batch_size=3, shuffle=True)
+    for (data, label) in temp_dataloader:
+        data = Variable(data)
+        out = model(data.float())
+        _, predicted = torch.max(out.data, 1)
+        total+=label.size(0)
+        correct+=(predicted==label).sum().item()
+    return correct/total
 
 def compute_accuracy(dataset, model):
     total = len(dataset)
-    temp_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    temp_dataloader = DataLoader(dataset, batch_size=3, shuffle=True)
     model.eval()
     total = len(dataset)
     correct = 0 
-    
     for (data,label) in temp_dataloader:
         data = Variable(data)
         out = model(data.float())
@@ -27,16 +55,36 @@ def compute_accuracy(dataset, model):
         predicted = predicted.numpy()
         label = label.numpy()
         for i in range(len(label)):
+            print("Predicted:\t",predicted[i],'\t',"Label:\t",label[i])
             if predicted[i] == label[i]:
                 correct+=1
             else:
-                print(predicted[i], label[i])
+                pass
+                #print(predicted[i], label[i])
     return correct/total
+
+def draw_loss(train_loss, valid_loss):
+    plt.plot(train_loss, label = 'train loss')
+    plt.plot(valid_loss, label = 'valid loss')
+    plt.xlabel("Epoch")
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+def draw_acc(train_acc, valid_acc):
+    plt.plot(train_acc, label = "train acc")
+    plt.plot(valid_acc, label = "valid acc")
+    plt.xlabel("Epoch")
+    plt.ylabel("Acc")
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 def training():
     # prepare dataset
-    all_dataset = HRMdataset("training_data/training.csv")
-    train_size = int(0.7*len(all_dataset))
+    all_dataset = HRMdataset("training_data/training-4class-from0.csv")
+    train_size = int(0.67*len(all_dataset))
     test_size = len(all_dataset)-train_size
     train_dataset, test_dataset = torch.utils.data.random_split(all_dataset, [train_size, test_size], generator=torch.Generator().manual_seed(40))
 
@@ -52,35 +100,62 @@ def training():
     train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)    
 
-    
     # define model 
-    model = NN(input_features=101, class_num=3)
+    model = NN(input_features=101, class_num=class_number)
 
     # optimizer & Loss function
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = nn.CrossEntropyLoss()
     loss_record = []
+    valid_record = []
+    train_acc_record = []
+    valid_acc_record = []
+
     # start training 
     for e in range(epochs):
         progress_bar = qqdm(train_dataloader)
+        running_loss = 0.0
         for (data, label) in progress_bar:
             data = Variable(data)
             out = model(data.float())
 
             loss = criterion(out, label.long())
-
+            acc = metric_acc(out, label.long())
+            
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            progress_bar.set_infos({
-                "Loss":round(loss.item(),7),
-                "Epoch":e+1,
-            })
+            running_loss+=loss.item()*data.size(0)
+            progress_bar.set_infos({"Loss":round(loss.item(),7), "Epoch":e+1,})
 
-    print("Train Acc:\t", compute_accuracy(train_dataset, model))
-    print("Test Acc:\t", compute_accuracy(test_dataset, model))
+        # validation (using testing data)
+        model.eval()
+        valid_loss = 0
+        for (data, label) in test_dataloader:
+            data = Variable(data)
+            out = model(data.float())
+            loss = criterion(out, label.long())
+            batch_loss = metric_valid_acc(out, label.long())
+            valid_loss+=loss.item()*data.size(0)
+        
+        total_train_acc = metric_acc.compute()
+        total_valid_acc = metric_valid_acc.compute()
+        train_acc_record.append(round(total_train_acc.cpu().item(), 4))
+        valid_acc_record.append(round(total_valid_acc.cpu().item(), 4))
+        metric_acc.reset()
+        metric_valid_acc.reset()
 
+        epoch_valid_loss = valid_loss / len(test_dataloader)
+        epoch_loss = running_loss / len(train_dataloader)
+        valid_record.append(epoch_valid_loss)
+        loss_record.append(epoch_loss)
+        model.train()
+
+    draw_loss(loss_record, valid_record)
+    draw_acc(train_acc_record, valid_acc_record)
+    print("Train Acc:\t", compute_accuracy2(train_dataset, model))
+    print("Test Acc:\t", compute_accuracy2(test_dataset, model))
 
 if __name__ == "__main__":
     training()
